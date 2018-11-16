8cab1e670d7faee1ce68c9095a0a84968376f039
ldk123456 <1786079189@qq.com>
2018-11-16 17:03:52 +0800

    README of Chapter 3

--- src/Chapter03/3.6_4_LocalFunctionsAndExtensions3.kt
+++ src/Chapter03/3.6_4_LocalFunctionsAndExtensions3.kt
@@ -6,7 +6,7 @@ fun User.validateBeforeSave(){
     fun validate(value: String, fieldName: String) {
         if (value.isEmpty()) {
             throw IllegalArgumentException(
-                "Can't save user $id: empty $fieldName")
+                "Can't save user $id: empty $fieldName")    //可以直接访问user的属性
         }
     }
 
@@ -15,7 +15,7 @@ fun User.validateBeforeSave(){
 }
 
 fun saverUser(user: User){
-    user.validateBeforeSave()
+    user.validateBeforeSave()       //调用扩展函数
 
     //保存user到数据库
 }
