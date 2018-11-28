187f6a34d7b345fd8a53b496705b68049e0f8c48
ldk123456 <1786079189@qq.com>
2018-11-28 17:29:19 +0800

    README of Chapter 4

--- src/Chapter04/4.4.3_ExtensionFunctionForCompanionObject.kt
+++ src/Chapter04/4.4.3_ExtensionFunctionForCompanionObject.kt
@@ -4,10 +4,10 @@ class Person(val firstName: String, val lastName: String) {
     companion object {}     //声明一个空的伴生对象
 }
 
-fun Person.Companion.fromJSON(json: String) : Person {  //声明一个扩展函数
+/*fun Person.Companion.fromJSON(json: String) : Person {  //声明一个扩展函数
     ...
-}
+}*/
 
 fun main(args: Array<String>) {
-    val p = Person.fromJSON(json)
+    //val p = Person.fromJSON(json)
 }
